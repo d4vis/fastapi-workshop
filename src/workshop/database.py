7108d6574045e021b3fa2698da9a29a0ad7c30f7
d4vis <dbukuru1@gmail.com>
2021-03-28 23:54:16 +0300

    added oauth2 authorization

--- src/workshop/database.py
+++ src/workshop/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .settings import settings
 
-engine = create_engine(settings.database_url, connect_args={'check_same_thread': False})
+engine = create_engine(settings.database_url, connect_args={
+                       'check_same_thread': False})
 
 
 Session = sessionmaker(engine, autocommit=False, autoflush=False)
--- src/workshop/settings.py
+++ src/workshop/settings.py
@@ -6,5 +6,9 @@ class Settings(BaseSettings):
     server_port: int = 8000
     database_url: str = 'sqlite:///./src/database.sqlite3'
 
+    jwt_secret: str
+    jwt_algorithm: str = 'HS256'
+    jwt_expiration: int = 3600
+
 
 settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
