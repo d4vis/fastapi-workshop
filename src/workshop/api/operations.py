983747e61ccd56b8e3bb0acd08f459618cbb0759
d4vis <dbukuru1@gmail.com>
2021-04-03 13:21:06 +0300

    Give access to API endpoints only for authorized users

--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends, Response, status
 
 from ..models.operations import Operation, OperationKind, OperationCreate, OperationUpdate
 from ..services.operations import OperationsService
+from ..services.auth import get_current_user
+from ..models.auth import User
 
 router = APIRouter(prefix='/operations')
 
@@ -10,40 +12,45 @@ router = APIRouter(prefix='/operations')
 @router.get('/', response_model=list[Operation])
 def get_operations(
         kind: Optional[OperationKind] = None,
+        user: User = Depends(get_current_user),
         service: OperationsService = Depends()
 ):
-    return service.get_list(kind=kind)
+    return service.get_list(user_id=user.id, kind=kind)
 
 
 @router.post('/', response_model=Operation)
 def create_operation(
     operation_data: OperationCreate,
+    user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
-    return service.create_operation(operation_data)
+    return service.create_operation(user_id=user.id, operation_data=operation_data)
 
 
 @router.get('/{operation_id}', response_model=Operation)
 def get_operation(
     operation_id: int,
+    user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
-    return service.get_operation(operation_id=operation_id)
+    return service.get_operation(user_id=user.id, operation_id=operation_id)
 
 
 @router.put('/{operation_id}', response_model=Operation)
 def update_operation(
     operation_id: int,
     operation_data: OperationUpdate,
+    user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
-    return service.update_operation(operation_id, operation_data)
+    return service.update_operation(user_id=user.id, operation_id=operation_id, operation_data=operation_data)
 
 
 @router.delete('/{operation_id}')
 def delete_operation(
     operation_id: int,
+    user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
-    service.delete_operation(operation_id)
+    service.delete_operation(user_id=user.id, operation_id=operation_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- src/workshop/services/operations.py
+++ src/workshop/services/operations.py
@@ -11,37 +11,37 @@ class OperationsService:
     def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
-    def _get(self, operation_id: int) -> tables.Operation:
+    def _get(self, user_id: int, operation_id: int) -> tables.Operation:
         operation = self.session.query(
-            tables.Operation).filter_by(id=operation_id).first()
+            tables.Operation).filter_by(id=operation_id, user_id=user_id).first()
         if not operation:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         return operation
 
-    def get_list(self, kind: Optional[OperationKind] = None) -> list[tables.Operation]:
-        query = self.session.query(tables.Operation)
+    def get_list(self, user_id: int, kind: Optional[OperationKind] = None) -> list[tables.Operation]:
+        query = self.session.query(tables.Operation).filter_by(user_id=user_id)
         if kind:
             query = query.filter_by(kind=kind)
         operations = query.all()
         return operations
 
-    def get_operation(self, operation_id: int) -> tables.Operation:
-        return self._get(operation_id)
+    def get_operation(self, user_id: int, operation_id: int) -> tables.Operation:
+        return self._get(user_id, operation_id)
 
-    def create_operation(self, operation_data: OperationCreate) -> tables.Operation:
-        operation = tables.Operation(**operation_data.dict())
+    def create_operation(self, user_id: int, operation_data: OperationCreate) -> tables.Operation:
+        operation = tables.Operation(**operation_data.dict(), user_id=user_id)
         self.session.add(operation)
         self.session.commit()
         return operation
 
-    def update_operation(self, operation_id: int, operation_data: OperationUpdate) -> tables.Operation:
-        operation = self._get(operation_id)
+    def update_operation(self, user_id: int, operation_id: int, operation_data: OperationUpdate) -> tables.Operation:
+        operation = self._get(user_id, operation_id)
         for field, values in operation_data:
             setattr(operation, field, values)
         self.session.commit()
         return operation
-    
-    def delete_operation(self, operation_id: int):
-        operation = self._get(operation_id)
+
+    def delete_operation(self, user_id: int, operation_id: int):
+        operation = self._get(user_id, operation_id)
         self.session.delete(operation)
         self.session.commit()
