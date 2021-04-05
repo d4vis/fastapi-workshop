1fdb4835f32e263446cc6d5c9f2e138eceb66850
d4vis <dbukuru1@gmail.com>
2021-04-05 22:16:09 +0300

    Update API documentation

--- src/workshop/api/auth.py
+++ src/workshop/api/auth.py
@@ -3,20 +3,56 @@ from fastapi.security import OAuth2PasswordRequestForm
 from ..models.auth import UserCreate, Token, User
 from ..services.auth import AuthService, get_current_user
 
-router = APIRouter(prefix='/auth')
+router = APIRouter(prefix='/auth', tags=['auth'])
 
 
 @router.post('/sign-up', response_model=Token)
-def sign_up(user_data: UserCreate, service: AuthService = Depends()):
+def sign_up(
+    user_data: UserCreate,
+    service: AuthService = Depends(),
+):
+    """
+    Регистрация пользователя
+    \f
+
+    Args:
+        user_data (UserCreate):
+        service (AuthService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.register_new_user(user_data)
 
 
 @router.post('/sign-in', response_model=Token)
-def sign_in(form_data: OAuth2PasswordRequestForm = Depends(),
-            service: AuthService = Depends()):
+def sign_in(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    service: AuthService = Depends(),
+):
+    """
+    Авторизация пользователя
+    \f
+
+    Args:
+        form_data (OAuth2PasswordRequestForm, optional): Defaults to Depends().
+        service (AuthService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.authenticate_user(form_data.username, form_data.password)
 
 
 @router.get('/user', response_model=User)
-def get_user(user: User = Depends(get_current_user)):
+def get_user(
+    user: User = Depends(get_current_user),
+):
+    """
+    Получить данные пользователя из токена
+    \f
+
+    Args:
+        user (User, optional): Defaults to Depends(get_current_user).
+
+    Returns:
+    """
     return user
--- src/workshop/api/operations.py
+++ src/workshop/api/operations.py
@@ -6,7 +6,7 @@ from ..services.operations import OperationsService
 from ..services.auth import get_current_user
 from ..models.auth import User
 
-router = APIRouter(prefix='/operations')
+router = APIRouter(prefix='/operations', tags=['operations'])
 
 
 @router.get('/', response_model=list[Operation])
@@ -15,6 +15,18 @@ def get_operations(
         user: User = Depends(get_current_user),
         service: OperationsService = Depends()
 ):
+    """
+    Получение списка операций
+
+    - **kind**: Фильтр по виду операций.
+    \f
+    Args:
+        kind (Optional[OperationKind], optional): Defaults to None.
+        user (User, optional): Defaults to Depends(get_current_user).
+        service (OperationsService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.get_list(user_id=user.id, kind=kind)
 
 
@@ -24,6 +36,17 @@ def create_operation(
     user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
+    """
+    Создать операцию
+    \f
+
+    Args:
+        operation_data (OperationCreate):
+        user (User, optional): Defaults to Depends(get_current_user).
+        service (OperationsService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.create_operation(user_id=user.id, operation_data=operation_data)
 
 
@@ -33,6 +56,18 @@ def get_operation(
     user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
+    """
+    Получить операцию по id
+    - **operation_id**: id операции.
+    \f
+
+    Args:
+        operation_id (int):
+        user (User, optional): Defaults to Depends(get_current_user).
+        service (OperationsService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.get_operation(user_id=user.id, operation_id=operation_id)
 
 
@@ -43,6 +78,18 @@ def update_operation(
     user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
+    """
+    Изменить операцию по id
+    - **operation_id**: id операции.
+    \f
+    Args:
+        operation_id (int):
+        operation_data (OperationUpdate):
+        user (User, optional): Defaults to Depends(get_current_user).
+        service (OperationsService, optional): Defaults to Depends().
+
+    Returns:
+    """
     return service.update_operation(user_id=user.id, operation_id=operation_id, operation_data=operation_data)
 
 
@@ -52,5 +99,17 @@ def delete_operation(
     user: User = Depends(get_current_user),
     service: OperationsService = Depends()
 ):
+    """
+    Удалить операцию по id
+    - **operation_id**: id операции.
+    \f
+
+    Args:
+        operation_id (int):
+        user (User, optional): Defaults to Depends(get_current_user).
+        service (OperationsService, optional): Defaults to Depends().
+
+    Returns:
+    """
     service.delete_operation(user_id=user.id, operation_id=operation_id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
